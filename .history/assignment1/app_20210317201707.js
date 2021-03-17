7d583962c1f350e55a076ad5b7bf460e6469f3c4
ThrasyvoulosKafasis <thrasyvoulos.kafasis@gmail.com>
2021-03-17 21:14:00 +0200

    Remove history folder from git

--- .history/assignment1/app_20210317201707.js
+++
@@ -1,9 +0,0 @@
-angular.module('LunchChecker', []);
-
-angular
-    .module('LunchChecker')
-    .controller('LunchCheckerController', LunchCheckerController);
-
-var LunchCheckerController = function () {
-
-}
